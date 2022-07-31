8ba982d2592488f1e088e6823647e9e753677b5b
Marco Santos <101365429+mscode98@users.noreply.github.com>
2022-08-01 00:29:27 +0100

    code refactoring

--- server.c
+++ server.c
@@ -1,68 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 
-void print_errono_exit(const char *step)
-{
-    fprintf(stderr, "ERROR: %s [%s]\n", strerror(errno), step);
-    exit(1);
-}
+#define TRY(som, step, msg, _exit) __extension__({                     \
+    int result = 0;                                                    \
+    do                                                                 \
+    {                                                                  \
+        result = som;                                                  \
+        if (result == -1)                                              \
+        {                                                              \
+            fprintf(stderr, "ERROR[%s]: %s\n", step, strerror(errno)); \
+            if (_exit)                                                 \
+                exit(1);                                               \
+        }                                                              \
+        else                                                           \
+            printf("[%s] %s\n", step, msg);                            \
+    } while (0);                                                       \
+    result;                                                            \
+})
 
-void print_errono(const char *step)
+void start_server(int socket_port)
 {
-    fprintf(stderr, "ERROR: %s [%s]\n", strerror(errno), step);
+    int sock = TRY(socket(AF_INET, SOCK_STREAM, 0), "Create", "Socket created.", true);
+
+    const struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(socket_port),
+        .sin_addr.s_addr = INADDR_ANY,
+    };
+
+    TRY(bind(sock, (const struct sockaddr *)&addr, sizeof(addr)), "Bind", "Socket binded.", true);
+    TRY(listen(sock, 80), "Listen", "Listenning...", false);
+
+    printf("Listening on port %d\n", addr.sin_port);
+
+    struct sockaddr_in client_socket = {0};
+    int client_socket_sz = sizeof(client_socket);
+
+    TRY(accept(sock, (struct sockaddr *)&client_socket, (socklen_t *)&client_socket_sz), "Client Connect", "Client Connected", false);
+
+    TRY(shutdown(sock, SHUT_RDWR), "Shutdown", "Success shutdown.", false);
+
+    close(sock);
 }
 
 int main(void)
 {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
-        print_errono_exit("Create");
-    else
-        printf("Success socket connection! %d \n", sock);
 
-    const struct sockaddr_in addr = {
-        .sin_family = AF_INET,
-        .sin_port = htons(6969),
-        .sin_addr.s_addr = INADDR_ANY,
-    };
-
-    int sockbind = bind(sock, (const struct sockaddr *)&addr, sizeof(addr));
-    if (sockbind == -1)
-        print_errono_exit("Bind");
-    else
-        printf("Success socket bind connection!\n");
-
-    int listens = listen(sock, 80);
-    if (listens == -1)
-    {
-        print_errono("Listen");
-    }
-    else
-    {
-        printf("Listening on port %d\n", addr.sin_port);
-        struct sockaddr_in tmp = {0};
-        int tmp_size = sizeof(tmp);
-        int next = accept(sock, (struct sockaddr*)&tmp, (socklen_t *)&tmp_size);
-        if (next == -1)
-        {
-            print_errono("Accept");
-        }
-        else
-        {
-            printf("Client connected! %d\n", tmp.sin_port);
-        }
-    }
-
-    int shutdown_ret = shutdown(sock, SHUT_RDWR);
-    if (shutdown_ret == -1)
-        print_errono("Shutdown");
-    else
-        printf("Success closed socket. %d\n", shutdown_ret);
-
-    close(sock);
+    start_server(6969);
 }

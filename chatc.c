e62be83305c70fc54fa679392129b3ff9e8c1f1a
Marco Santos <101365429+mscode98@users.noreply.github.com>
2022-08-01 01:48:44 +0100

    Rename client_gui to chatc

---
+++ chatc.c
@@ -0,0 +1,22 @@
+#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+#include "client.h"
+
+int main(void)
+{
+    connect_client(6969);
+
+    // SetTraceLogLevel(LOG_NONE);
+    // InitWindow(400, 400, "Chatc");
+
+    // while (!WindowShouldClose())
+    // {
+    //     BeginDrawing();
+       
+    //     EndDrawing();
+    // }
+
+    // CloseWindow();
+}
